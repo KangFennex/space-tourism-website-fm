1fd01c00bd7eccf02959f147bc8483ec40dfc36a
Michel Marion <kangkm19@gmail.com>
2024-02-12 17:34:42 +0000

    Fiex transitions

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -41,29 +41,42 @@ const Crew = () => {
         animate="final"
         exit="exit"
         className="crew__container">
-        <div className="crew__details">
+        <motion.div
+          key={selectedCrew.name}
+          initial={{ opacity: 0.3 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0.5 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="crew__details">
           <h4>{selectedCrew.role}</h4>
           <h1>{selectedCrew.name}</h1>
           <p>{selectedCrew.bio}</p>
           <nav className="crew__details__tabs">
             <ul>
-        {data.crew.map((crew, index) => (
-          <li
-          key={index}
-          className={`${selectedCrew === crew ? "selected" : ""}`}
-          onClick={() => handleSelectCrew(crew)}
-          ></li>
-        ))}
+              {data.crew.map((crew, index) => (
+                <li
+                  key={index}
+                  className={`${selectedCrew === crew ? "selected" : ""}`}
+                  onClick={() => handleSelectCrew(crew)}
+                ></li>
+              ))}
             </ul>
           </nav>
-        </div>
+        </motion.div>
         <motion.div
           variants={variants}
           initial="initial"
           animate="final"
           exit="exit"
           className="crew__image">
-          <img src={selectedCrew.images.webp} alt={selectedCrew.name} />
+          <motion.img
+            src={selectedCrew.images.webp}
+            alt={selectedCrew.name}
+            key={selectedCrew.name}
+            initial={{ opacity: 0.5 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0.5 }}
+            transition={{ duration: 1, ease: "easeInOut" }} />
         </motion.div>
       </motion.div>
     </div>
--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -42,16 +42,23 @@ const Destination = () => {
         exit="exit"
         className="destination__container">
         <div className="destination__image">
-          <motion.img 
-          src={selectedPlanet.images.webp}
-          key={selectedPlanet.name}
-          initial={{ opacity: 0.5 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0.5 }}
-          transition={{ duration: 2, ease: "easeInOut" }}  
+          <motion.img
+            src={selectedPlanet.images.webp}
+            key={selectedPlanet.name}
+            initial={{ opacity: 0.5 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0.5 }}
+            transition={{ duration: 2, ease: "easeInOut" }}
           ></motion.img>
         </div>
-        <div className="destination__details">
+        <motion.div 
+        variants={variants}
+        key={selectedPlanet.name}
+        initial={{ opacity: 0.3 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0.5 }}
+        transition={{ duration: 1, ease: "easeInOut" }}
+        className="destination__details">
           <nav className="destination__details__tabs">
             <ul>
               {data.destinations.map((destination, index) => (
@@ -66,21 +73,20 @@ const Destination = () => {
             </ul>
           </nav>
 
-          <h1>{selectedPlanet.name}</h1>
-          <p>{selectedPlanet.description}</p>
-          <div className="destination__details__divider"></div>
-          <div className="destination__details__distance">
-            <div>
-              <h4>AVG. DISTANCE</h4>
-              <h2>{selectedPlanet.distance}</h2>
-            </div>
-            <div>
-              <h4>EST. TRAVEL TIME</h4>
-              <h2>{selectedPlanet.travel}</h2>
-            </div>
+            <h1>{selectedPlanet.name}</h1>
+            <p>{selectedPlanet.description}</p>
+            <div className="destination__details__divider"></div>
+            <div className="destination__details__distance">
+              <div>
+                <h4>AVG. DISTANCE</h4>
+                <h2>{selectedPlanet.distance}</h2>
+              </div>
+              <div>
+                <h4>EST. TRAVEL TIME</h4>
+                <h2>{selectedPlanet.travel}</h2>
+              </div>
           </div>
-
-        </div>
+        </motion.div>
       </motion.div>
     </div>
   )
